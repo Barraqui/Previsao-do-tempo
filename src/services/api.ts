56cffcbda699c314406007a8e2f3b95a0f253fb4
Barraqui <gabrielmartinsbarraqui@gmail.com>
2025-02-26 18:39:20 -0300

    Novas informações

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,6 @@ export  async function getWeatherData(city: string) {
     const apiKey = import.meta.env.VITE_APIKEY;
 
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
-
+    
     return (await fetch(apiWeatherURL)).json();
 }
--- src/services/types.ts
+++ src/services/types.ts
@@ -2,5 +2,8 @@ export interface Props {
     types: {
         cidade?: string
         temperatura?: number
+        descricao?: string
+        icone?: object
+        senTermica?: number
     }
 }
